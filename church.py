d6e707f3a7fb23573b41fbdca20c3ec95fee399b
gpib-420 <79027516+gpib-420@users.noreply.github.com>
2023-07-20 03:59:50 +0200

    Update church.py
    sdd

--- church.py
+++ church.py
@@ -16,11 +16,19 @@ def remove_class(element, class_name):
 def add_class(element, class_name):
     element.element.classList.add(class_name)
 
-def roomIdFromName(room_name):
-    return 0
+def roomIdFromName(input) {
+code_1 = input[0].charCodeAt(0)*2**32
+code_2 = input[1].charCodeAt(0)*2**16
+code_3 = input[2].charCodeAt(0)*2**0
+
+result = code_1+code_2+code_3
+
+return result
+}
 
 
-def openroom(room_name:str):
+}
+def openRoom(room_name:str):
     print('YOU ARE IN ROOM %s'%room_name)
     print('checking the database...')
     print('looking for room %s'%room_name+'with ID %s'%int(roomIdFromName(room_name)))
